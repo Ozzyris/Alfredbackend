9397a71dca8d75fb025d1c078dd1ac5f6619f19a
Alexandre Nicol <nemokervi@yahoo.fr>
2018-06-05 17:21:34 +1000

    ready to rock

--- backend/e2e/src/app.e2e-spec.ts
+++ backend/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('alfredbackend App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('alfredbackend App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to alfred-at-work!');
   });
 });
--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent{
-
+	constructor(){}
 	scroll_top() {
 	    window.scroll(0,0);
 	}
